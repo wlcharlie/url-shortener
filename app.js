bf8cdcadee0981a2edfab7626a940da0fbcc7737
wlcharlie <wl00606352@gmail.com>
2021-05-18 01:57:36 +0800

    minor changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const createProcess = require('./createProcess')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/url-shortener'
 mongoose.connect
   (MONGODB_URI, {
     useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
